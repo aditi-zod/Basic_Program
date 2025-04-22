e937a803f6507abaea41b840c38f88afa362e2e7
aditi.zod <aditi.zod@softude.com>
2025-04-22 19:34:15 +0530

    testing first program

---
+++ Myfirstprogram.py
@@ -0,0 +1,13 @@
+print("Hello Aditi!")
+print("How are you?")
+#exit()
+
+i=5
+if i > 2:
+
+ print("Five is greater than three!")
+if i > 2:
+        print("Five is greater than two!")
+else:
+        print("Five is lesser than two!")
+
